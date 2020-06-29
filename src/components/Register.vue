8a17bfff4ae16ed20895a50fd67c50b203a252b3
sorawich01 <sorawith085@hotmail.com>
2020-06-30 00:17:59 +0700

    add vue

---
+++ src/components/Register.vue
@@ -0,0 +1,47 @@
+
+<template>
+
+	<div>
+	<h1>Register</h1>
+
+	<input
+		type="email"
+		name="email"
+		v-model="email"
+		placeholder="email" />
+	<br>
+	<input
+		type="password"
+		name="password"
+		v-model="password"
+		placeholder="password" />
+	<br>
+	<button
+		@click="register">
+		Register
+	</button>
+	</div>
+</template>
+
+<script>
+
+// import AuthenticationService from '../servises/AuthenticationService.js'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+    watch: {
+  	  email (value) {
+  		  console.log('email changed ' , value)
+  	  }
+    },
+  	mounted () {
+	  	setTimeout(() => {
+			this.email = 'hello world'
+	  	},2000)
+  	}
+}
+</script>
---
+++ src/servises/AuthenticationService.js
@@ -0,0 +1,6 @@
+import Api from './Api';
+export default {
+	register (credentials){
+		return Api().post('register', credentials)
+	}
+}
